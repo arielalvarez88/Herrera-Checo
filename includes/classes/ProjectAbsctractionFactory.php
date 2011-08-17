9282ca404df67260cc347615130e023196682445
Ariel <ariel@ariel-desktop.(none)>
2011-08-17 03:55:38 -0400

    Adding require_once

--- includes/classes/ProjectAbsctractionFactory.php
+++ includes/classes/ProjectAbsctractionFactory.php
@@ -4,8 +4,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-require dirname(__FILE__).'/ResidentialProjectAbstraction.php';
-require dirname(__FILE__).'/ComercialProjectAbstraction.php';
+require_once  dirname(__FILE__).'/ResidentialProjectAbstraction.php';
+require_once  dirname(__FILE__).'/ComercialProjectAbstraction.php';
 class ProjectAbstractionFactory
 {
 
