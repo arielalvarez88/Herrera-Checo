1baa2b56a566f03f0501339438ce561c26282c2d
Ariel <ariel@ariel-desktop.(none)>
2011-08-18 07:12:09 -0400

    adding the links to proyectos comerciales and residenciales of front page

--- includes/blocks/proyectosComercialesResidenciales.php
+++ includes/blocks/proyectosComercialesResidenciales.php
@@ -3,6 +3,6 @@ require dirname(__FILE__) .'/../paths.php';
 
 ?>
 <div id="proyectos-comerciales-residenciales">
-    <a href="#not-yet"  id="proyectos-comerciales"><img src="<?php echo $paths->images;?>/proyectosComercialesResidenciales/proyectosComerciales.png"/></a>
-    <a href="#not-yet"><img src="<?php echo $paths->images;?>/proyectosComercialesResidenciales/proyectosResidenciales.png"/></a>
+    <a href="/proyectos-comerciales"  id="proyectos-comerciales"><img src="<?php echo $paths->images;?>/proyectosComercialesResidenciales/proyectosComerciales.png"/></a>
+    <a href="/proyectos-residenciales"><img src="<?php echo $paths->images;?>/proyectosComercialesResidenciales/proyectosResidenciales.png"/></a>
 </div>
