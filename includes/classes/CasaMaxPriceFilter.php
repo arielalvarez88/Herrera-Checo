208d660a691a4a3dd341bd92bc9d19d1f69e62d3
Ariel <ariel@ariel-desktop.(none)>
2011-08-19 08:05:04 -0400

    Fixing class name

--- includes/classes/CasaMaxPriceFilter.php
+++ includes/classes/CasaMaxPriceFilter.php
@@ -1,7 +1,7 @@
 <?php
 require_once dirname(__FILE__).'/IFilter.php';
 
-Class CasaMinPriceFilter implements IFilter{
+Class CasaMaxPriceFilter implements IFilter{
     public $fieldName;
     public $value;
     
