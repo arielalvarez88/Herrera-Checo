0660d51e06af11d89d26ebffb3a4a986fa1ef42d
Ariel <ariel@ariel-desktop.(none)>
2011-08-19 08:10:20 -0400

    Fixing Class names

--- includes/classes/CasaConditionFilter.php
+++ includes/classes/CasaConditionFilter.php
@@ -2,7 +2,7 @@
 
 require_once dirname(__FILE__).'/IFilter.php';
 
-Class ResidentialProjectLocalConditionFilter implements IFilter{
+Class CasaConditionFilter implements IFilter{
     public $fieldName;
     public $value;
     
