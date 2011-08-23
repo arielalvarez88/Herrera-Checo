c94c82654f57d0ef75257566cf3a764b409e521e
Ariel <ariel@ariel-desktop.(none)>
2011-08-23 07:58:16 -0400

    Headers in projects

---
+++ includes/blocks/proyectosResidencialesHeaderPortafolioPage.php
@@ -0,0 +1,1 @@
+<img id="residential-projects-header" src="/sites/default/files/images/proyecto/proyectosResidencialesHeader.png" alt="Proyectos Residenciales"/> <a href="/portafolio/proyectos-residenciales"><img id="show-all-residencial" src="/sites/default/files/images/proyecto/verTodos.png" alt="Proyectos Residenciales"/></a>
---
+++ includes/blocks/proyectosResidencialesHeaderProyectosPage.php
@@ -0,0 +1,1 @@
+<img id="residential-projects-header" src="/sites/default/files/images/proyecto/proyectosResidencialesHeader.png" alt="Proyectos Residenciales"/> <a href="/proyectos/proyectos-residenciales"><img id="show-all-residencial" src="/sites/default/files/images/proyecto/verTodos.png" alt="Proyectos Residenciales"/></a>
