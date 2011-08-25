ab9dd8f1c29f582754261ec013b0f00411d0a35a
Ariel <ariel@ariel-desktop.(none)>
2011-08-25 01:33:09 -0400

    fixing nosotros style

--- includes/blocks/employees.php
+++ includes/blocks/employees.php
@@ -1,15 +1,19 @@
 <div id="employees">
+    <h2 id="employees-title">Nuestro Equipo</h2>
     <?php for ($i = 0; $i < 3; $i++): ?>
     
-        <h2 id="employees-title">Nuestro Equipo</h2>
+        
         <div class="employees-horizontal-divisor"></div>
         <div class="employee">
             <img class="employee-photo" src="/sites/default/files/images/employees/defaultEmployeePhoto.png"/>
-            <p class="employee-info">
-            <h3>ING. Nombre del empleado</h3>
-            <h6>Puesto</h6>
-            Tonight we gonna get on the flor
-            </p>
+            <div class="employee-text">
+                <h3>ING. Nombre del empleado</h3>
+                <h6>Puesto</h6>
+                <p class="employee-info"> Descripci&oacute;n del puesto.</p>
+            </div>
+                
+                
+            
         </div>
     <?php endfor; ?>
 
