28bd62245e4d14f8b5112b69240b1cebaf261a3b
Ariel <ariel@ariel-desktop.(none)>
2011-08-27 02:28:32 -0400

    Fixing

--- includes/views/project_view.php
+++ includes/views/project_view.php
@@ -102,7 +102,7 @@ $viewsParams['projectsAbstractions'] = $relatedProjects;
                 <div id="proyecto-atractivos">
                     <h3>Atractivos</h3>
                     <div id="proyecto-atractivos-text">
-                        <?php $projectAbstraction->getAtractives();?>
+                        <?php echo $projectAbstraction->getAtractives();?>
                     </div>
                     <div class="proyecto-shadow">
                         <img alt="sombra" src="<?php echo $paths->images; ?>/proyecto/shadow.png" />
