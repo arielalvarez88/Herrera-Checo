13b5b01ab4be778b01c4ec4736ef0a485e0ca19e
Ariel <ariel@ariel-desktop.(none)>
2011-09-19 17:21:31 -0400

    adding maintenancePage view

---
+++ includes/views/maintenancePage.php
@@ -0,0 +1,12 @@
+<?php require dirname(__FILE__).'/../paths.php'?>
+<html>
+    <head>
+        <title>Constructora Herrara-Checo</title>
+    </head>
+    <body>
+        <div id="maintenance-img-container" style="text-align: center;">
+            <img src="<?php echo $paths->images;?>/common/maintenance.gif"/>
+        </div>
+    </body>
+    
+</html>
