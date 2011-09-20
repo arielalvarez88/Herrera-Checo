75dabf9689680eed2fdd3212b83b6c83b192f888
Ariel <ariel@ariel-desktop.(none)>
2011-09-20 11:07:37 -0400

    background map smaller

--- includes/blocks/proyectosComercialesResidenciales.php
+++ includes/blocks/proyectosComercialesResidenciales.php
@@ -3,6 +3,6 @@ require dirname(__FILE__) .'/../paths.php';
 
 ?>
 <div id="proyectos-comerciales-residenciales">
-    <a href="/proyectos-comerciales"  id="proyectos-comerciales"><img src="<?php echo $paths->images;?>/proyectosComercialesResidenciales/proyectosComerciales.gif"/></a>
-    <a href="/proyectos-residenciales"><img src="<?php echo $paths->images;?>/proyectosComercialesResidenciales/proyectosResidenciales.gif"/></a>
+    <a href="/proyectos-comerciales"  id="proyectos-comerciales"><img src="<?php echo $paths->images;?>/proyectosComercialesResidenciales/proyectosComerciales.png"/></a>
+    <a href="/proyectos-residenciales"><img src="<?php echo $paths->images;?>/proyectosComercialesResidenciales/proyectosResidenciales.png"/></a>
 </div>
