e6fd78a0e29da1728c26bfac23154e9f91611f01
Ariel <ariel@ariel-desktop.(none)>
2011-09-30 13:29:16 -0400

    suscribete

--- includes/blocks/newsletter.php
+++ includes/blocks/newsletter.php
@@ -1,5 +1,5 @@
 <div id="newsletter">
     <h2>Newsletter</h2>
     <p>Recibe las actualizaciones de nuestros &uacute;ltimos proyectos.</p>
-    <input type="text"/><span id="nesletter-submit">Subscr&iacute;bete</span>
+    <input type="text"/><span id="nesletter-submit">Suscr&iacute;bete</span>
 </div>
