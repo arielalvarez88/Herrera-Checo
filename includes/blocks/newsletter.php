1c8373395ecf3d536dad667f5a27d36c5c0c87d6
unknown <Ariel@.(none)>
2011-10-22 12:40:16 -0400

    Fixing newsletter subscribe

--- includes/blocks/newsletter.php
+++ includes/blocks/newsletter.php
@@ -1,5 +1,8 @@
 <div id="newsletter">
     <h2>Newsletter</h2>
     <p>Recibe las actualizaciones de nuestros &uacute;ltimos proyectos.</p>
-    <input type="text"/><span id="nesletter-submit">Suscr&iacute;bete</span>
+    <div id="newsletter-email-container">
+        <input type="text">
+        <span id="newsletter-submit">Suscríbete</span>
+    </div>
 </div>
